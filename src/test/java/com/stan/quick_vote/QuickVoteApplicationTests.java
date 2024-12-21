0a79d913acbc784a192162e03b64418bb266f44d
Stanrley Silva <stanrley.silva@tegrus.com.br>
2024-12-21 13:02:14 -0300

    Initial commit

---
+++ src/test/java/com/stan/quick_vote/QuickVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stan.quick_vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
