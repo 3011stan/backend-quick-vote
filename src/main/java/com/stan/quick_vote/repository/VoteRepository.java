496dfbb90f84b48c77a3a23a4ee156515ffe69eb
Stanrley Silva <stanrley.silva@tegrus.com.br>
2024-12-23 12:18:53 -0300

    feat: create logic to get voting session result

--- src/main/java/com/stan/quick_vote/repository/VoteRepository.java
+++ src/main/java/com/stan/quick_vote/repository/VoteRepository.java
@@ -3,7 +3,9 @@ package com.stan.quick_vote.repository;
 import com.stan.quick_vote.model.Vote;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface VoteRepository extends MongoRepository<Vote, String> {
     boolean existsByTopicIdAndAssociateId(String topicId, String associateId);
-
+    List<Vote> findAllByVotingSessionId(String votingSessionId);
 }
--- src/main/java/com/stan/quick_vote/repository/VotingSessionRepository.java
+++ src/main/java/com/stan/quick_vote/repository/VotingSessionRepository.java
@@ -4,7 +4,9 @@ import com.stan.quick_vote.model.VotingSession;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VotingSessionRepository extends MongoRepository<VotingSession, String> {
-
+    Optional<VotingSession> findById(String id);
 }
