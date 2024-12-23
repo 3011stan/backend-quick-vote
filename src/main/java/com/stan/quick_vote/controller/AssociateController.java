c5541f74b79fd3aeea9bbda1fc228c3cfb6da123
Stanrley Silva <stanrley.silva@tegrus.com.br>
2024-12-23 12:36:42 -0300

    feat: basic config swagger

--- src/main/java/com/stan/quick_vote/controller/AssociateController.java
+++ src/main/java/com/stan/quick_vote/controller/AssociateController.java
@@ -3,6 +3,8 @@ package com.stan.quick_vote.controller;
 import com.stan.quick_vote.dto.AssociateRequestDTO;
 import com.stan.quick_vote.dto.AssociateResponseDTO;
 import com.stan.quick_vote.service.AssociateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/associates")
+@Tag(name = "Associates", description = "Manage associates")
 public class AssociateController {
     private final AssociateService associateService;
 
     @PostMapping
-    public AssociateResponseDTO createAssociate(@RequestBody @Valid AssociateRequestDTO associateRequestDTO) {
+    @Operation(summary = "Create a new associate", description = "Adds a new associate to the system")
+    public AssociateResponseDTO createAssociate(@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Associate details")
+                                                @RequestBody @Valid AssociateRequestDTO associateRequestDTO) {
         return associateService.createAssociate(associateRequestDTO);
     }
 }
